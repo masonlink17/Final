236372543aab10bb9364da1bdcf93b8d3aa71ced
Mason <mlink@students.ccu.edu>
2024-05-07 09:46:22 -0600

    1/4

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Net;
+
+while (true)
+{
+    if (DateTime TryParse(response, out ValueType birthday))
+    {
+        var response = Console.ReadLine();
+        string dateInput = response;
+        var parsedDate = DateTime.Parse(dateInput);
+        Console.WriteLine(parsedDate);
+        break;
+    }
+    else
+    {
+        continue;
+    }
+
+}
